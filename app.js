7537dd5f72b41273862bdacaba5db9adc09dc6cd
isekai <flocoaraze06390@hotmail.fr>
2024-10-07 16:07:47 +0200

    update

---
+++ app.js
@@ -0,0 +1,33 @@
+let currentIndex = 0;
+
+function testNav() {
+  if (currentIndex !== 0) { // Si tu a deja selectionner un item alors je desactive l'ancien item (Ici représenter par un background color)
+    let lastButton = document.getElementById(currentIndex);
+    lastButton.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+  };
+  currentIndex++; // Jajoute +1 a mon index
+  let selectecButton = document.getElementById(currentIndex);
+  if (selectecButton) { // Si l'item avec mon currentIndex existe alors je le selection (Ici montrer par le bckground color)
+    selectecButton.style.backgroundColor = "red";
+  } else { // sinon je revien a mon index de depart
+    currentIndex = 0;
+    testNav();
+  }
+}
+
+function testSel() {
+  if (currentIndex !== 0) { // Si currentIndex est différent de 0 (donc qu'un item est sélectionné)
+    const selectecButton = document.getElementById(currentIndex);
+    if (selectecButton) { // par sécurité on verifie que le bouton existe bien
+      selectecButton.onclick(); // j'effectue l'action defini pour mon bouton
+    }
+  }
+}
+
+function onClickitem(buttonId) {
+  console.log("tu vien d'appuyer sur le bouton avec pour id "+buttonId)
+}
+
+document.addEventListener('keyup', function(event) {
+    console.log("tu a appuyer sur la touche "+event.key)
+});
