f8010ee32ad73425a6f2c79f0eaa7ab4df90fa18
Feulow <flocoaraze06390@hotmail.fr>
2024-10-07 21:55:48 +0200

    update

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ let currentIndex = 0;
 function testNav() {
   if (currentIndex !== 0) { // Si tu a deja selectionner un item alors je desactive l'ancien item (Ici représenter par un background color)
     let lastButton = document.getElementById(currentIndex);
-    lastButton.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+    lastButton.style.transform = "scale(1)";
+    lastButton.style.boxShadow = "0 4px 8px rgba(66, 66, 66, 0.137)";
   };
   currentIndex++; // Jajoute +1 a mon index
   let selectecButton = document.getElementById(currentIndex);
   if (selectecButton) { // Si l'item avec mon currentIndex existe alors je le selection (Ici montrer par le bckground color)
-    selectecButton.style.backgroundColor = "red";
+    selectecButton.style.transform = "scale(1.05)";
+    selectecButton.style.boxShadow = "0 4px 8px rgba(0, 255, 0, 0.137)";
   } else { // sinon je revien a mon index de depart
     currentIndex = 0;
-    testNav();
+    testNav(); 
   }
 }
 
